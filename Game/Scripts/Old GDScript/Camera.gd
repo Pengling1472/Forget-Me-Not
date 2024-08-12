d68260f72d1f87352b2b0d07d752f6620427357c
Pengling1472 <60308403+Pengling1472@users.noreply.github.com>
2024-08-11 17:23:49 -0700

    Delete Game/Scripts/Old GDScript directory

--- Game/Scripts/Old GDScript/Camera.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-@warning_ignore("unused_parameter")
-func _physics_process( delta ):
-	
-	global_position = get_node( "../MainCharacter" ).global_position
-	force_update_scroll()
